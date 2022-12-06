c367378a1803a742c12878b771d5faf4566366e1
ChillerK5 <klimbagr@mail.ru>
2022-12-07 03:20:13 +0700

    Tests hotfix

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,4 @@
-import {render} from '@testing-library/react'
-import App from "./App";
-
 test('Renders default text', () => {
-    render(<App />);
     const test = true;
     expect(test).toBe(true);
 })
