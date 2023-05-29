81b919eb6d7024cda2773cabff56fb63c84b0a61
Mikhail05Evdokimov <91541224+Mikhail05Evdokimov@users.noreply.github.com>
2023-05-29 13:47:58 +0700

    Users List as in VK

--- src/pages/profile-page/content/treasurer-options/content/UserCard.js
+++ src/pages/profile-page/content/treasurer-options/content/UserCard.js
@@ -1,19 +1,11 @@
-import { useDispatch } from "react-redux";
 import {
   MDBBadge,
   MDBBtn,
-  MDBBtnGroup,
-  MDBCard,
-  MDBCardImage,
-  MDBCardText,
-  MDBCardTitle,
   MDBCol,
   MDBIcon,
-  MDBRow,
 } from "mdb-react-ui-kit";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ChangeBalanceDialog from "./ChangeBalanceDialog";
-//import { changeBalanceModal, setChangeBalanceModal } from "../TreasurerPageTab";
 
 /**
  * Storage item card.
@@ -24,44 +16,33 @@ import ChangeBalanceDialog from "./ChangeBalanceDialog";
  * @constructor
  */
 function UserCard(props) {
-  const dispatch = useDispatch();
   const user = props.user.user;
-  const [cartQuantity, setCartQuantity] = useState(0);
-
+  const [balance, setBalance] = useState(user.user_money);
   const [changeBalanceModal, setChangeBalanceModal] = useState(false);
 
   return (
     <div>
-    <ChangeBalanceDialog isActive={changeBalanceModal} setIsActive={setChangeBalanceModal}
-     currentUserName={user.user_name} currentUserID={user.user_id}/>
-    <MDBCard className={"h-100 p-3"} >
-      <MDBRow>
-        <MDBCol size={"10"} className={"ps-0"}>
-          <MDBCardTitle className={"text-truncate"}>
-          
-    <h5>
-    {user.user_name + " "}
-      <MDBBadge
-        className={`bage`}
-        pill
-        color={"secondary"}
-      >
-        {user.user_money}
-      </MDBBadge>
-    </h5> 
-
-          </MDBCardTitle>
+      <ChangeBalanceDialog setBalance={setBalance} isActive={changeBalanceModal} setIsActive={setChangeBalanceModal}
+     currentUserName={user.user_name} currentUserID={user.user_login}/>
+    
+      <MDBCol size={"12"} className={"ps-0"}>   
+        <h5>
+          {user.user_name + " "}
+          <MDBBadge
+          pill
+          color={"secondary"}
+          >
+            {balance}
+          </MDBBadge>
+        </h5> 
           <MDBBtn
             color={"dark"}
-            hidden={cartQuantity !== 0}
             onClick={() => setChangeBalanceModal(!changeBalanceModal)}
           >
             Изменить баланс
             <MDBIcon className={"ps-2"} fas size={"lg"} icon={"coins"} />
           </MDBBtn>
         </MDBCol>
-      </MDBRow>
-    </MDBCard>
     </div>
   );
 }
