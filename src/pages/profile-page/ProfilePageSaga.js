7d3c5a0e7091a255439748b55455cbc1e88bcfbb
Mikhail05Evdokimov <91541224+Mikhail05Evdokimov@users.noreply.github.com>
2023-05-30 00:11:55 +0700

    delete request body in saga

--- src/pages/profile-page/ProfilePageSaga.js
+++ src/pages/profile-page/ProfilePageSaga.js
@@ -1,97 +1,40 @@
-import { call, put, takeEvery } from "redux-saga/effects";
-import { execApiCall } from "../../utils/ApiUtils";
-import {
-  removeMyItemsByTypeById,
-  updateItemsByType,
-  updateMyItemsByType,
-  updateUserData,
-} from "./ProfilePageSlice";
-import { saveUserRoles } from "../../api/Cookie";
-import {
-  getItemsInUseHistoryAction,
-  getMyItemsInUseHistoryAction,
-  getMyProfileAction,
-  unbookItemsAction,
-} from "./ProfilePageActions";
-import { apiGetMyProfile } from "../../api/auth/ApiCalls";
-import {
-  apiGetItemsInUseHistory,
-  apiUnbookItems,
-} from "../../api/models/ApiCalls";
-import { createSuccessToast } from "../../models/ToastModel";
+import { call, takeEvery, put } from "redux-saga/effects";
+import { execApiCall } from "../../../../utils/ApiUtils";
+import { setAllUsers } from "./TreasurerPageSlice";
+import { getAllUsersAction } from "./TreasurerPageActions";
+import { apiGetAllUsers, apiModifyUserBalance } from "../../../../api/models/ApiCalls";
+import LoadingState from "../../../../enums/LoadingState";
+import { createSuccessToast } from "models/ToastModel";
+import { modifyUserBalanceAction } from "./TreasurerPageActions";
+import { createErrorToast } from "models/ToastModel";
 
-export function* profilePageSagaWatcher() {
-  yield takeEvery(getMyProfileAction, sagaGetMyProfile);
-  yield takeEvery(getItemsInUseHistoryAction, sagaGetItemsInUseHistory);
-  yield takeEvery(getMyItemsInUseHistoryAction, sagaGetMyItemsInUseHistory);
-  yield takeEvery(unbookItemsAction, sagaUnbookItems);
+export function* treasurerPageSagaWatcher() {
+  yield takeEvery(getAllUsersAction, sagaGetAllUsers);
+  yield takeEvery(modifyUserBalanceAction, sagaModifyUserBalance);
 }
 
-function* sagaGetMyProfile() {
+function* sagaGetAllUsers(action) {
   yield call(execApiCall, {
-    mainCall: () => apiGetMyProfile(),
+    mainCall: () => apiGetAllUsers(action.payload),
     *onSuccess(response) {
-      // TODO Will be deprecated when user roles api call is patched
-      saveUserRoles(response.data.user.user_roles);
-      yield put(updateUserData(response.data.user));
+      const data = response.data;
+      yield put(setAllUsers({ data: data.users, status: LoadingState.LOADED }));
+    },
+    *onAnyError() {
+      yield put(setAllUsers({ data: [], status: LoadingState.ERROR }));
+      //yield createErrorToast("He удалось загрузить данные пользователей");
     },
   });
 }
 
-function* sagaUnbookItems(action) {
-  const requestBody = {
-    use_ids: action.payload.map((item) => {
-      return {
-        quantity: item.item_quantity,
-        use_id: item.use_id,
-      };
-    }),
-  };
-
+function* sagaModifyUserBalance(action) {
   yield call(execApiCall, {
-    mainCall: () => apiUnbookItems(requestBody),
+    mainCall: () => apiModifyUserBalance(action.payload.user.user_login, action.payload),
     *onSuccess() {
-      // TODO Fix item_id to use_id
-      createSuccessToast("Бронь отменена");
-      yield put(
-        removeMyItemsByTypeById({
-          type: "booked",
-          data: action.payload.map((i) => i.item_id),
-        })
-      );
+      createSuccessToast("Баланс обновлён");
     },
-  });
-}
-
-function* sagaGetItemsInUseHistory(action) {
-  yield call(execApiCall, {
-    mainCall: () => apiGetItemsInUseHistory(action.payload),
-    *onSuccess(response) {
-      yield put(
-        updateItemsByType({ type: action.payload, data: response.data })
-      );
-    },
-  });
-}
-
-function* sagaGetMyItemsInUseHistory(action) {
-  yield call(execApiCall, {
-    mainCall: () => apiGetMyProfile(),
-    *onSuccess(response) {
-      let items = response.data.user.items;
-      switch (action.payload) {
-        case "requested":
-          items = items.filter((i) => i.is_confirm === 0);
-          break;
-        case "booked":
-          items = items.filter((i) => i.is_confirm === 1);
-          break;
-        case "taken":
-          items = items.filter((i) => i.is_confirm === 2);
-          break;
-      }
-      items = items.filter((i) => i.item_quantity > 0);
-      yield put(updateMyItemsByType({ type: action.payload, data: items }));
+    *onAnyError() {
+      yield createErrorToast("He удалось выполнить запрос");
     },
   });
 }
